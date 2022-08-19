cabf78e96e155ac5dc1818af501a808b81815e7e
mkhan42 <michallekhan9@gmail.com>
2022-08-18 21:46:41 -0400

    added notes

--- DS-PRACTICE-COLT-STEELE/addUpTo.js
+++ DS-PRACTICE-COLT-STEELE/addUpTo.js
@@ -10,9 +10,22 @@ function addUpTo(n) {
 
 console.log(addUpTo(6))
 
+let t1 = performance.now();
+addUpTo(1000000000)
+let t2 = performance.now
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+//1.257500000006985 seconds
+//changes every time
+
+
 // Second Way (shorter method)
 function addUpToSecond(n) {
     return n * (n + 1) / 2;
 }
 
 console.log(addUpToSecond(6))
+
+let t3 = performance.now();
+addUpToSecond(1000000000)
+let t4 = performance.now
+console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`)
