f36e50f4b32e3b75efdb8281f341f46514652f01
mkhan42 <michallekhan9@gmail.com>
2022-08-24 00:33:16 -0400

    classes and pointers practice"

---
+++ DS-PRACTICE-SCOTT-BARRETT/cookie.js
@@ -0,0 +1,17 @@
+class Cookie {
+    constructor(color) {
+        this.color = color
+    }
+    getColor() {
+        return this.color
+    }
+    setColor(color) {
+        this.color = color
+    }
+}
+ 
+let cookieOne = new Cookie('green')
+let cookieTwo = new Cookie('blue')
+
+console.log(cookieOne)
+console.log(cookieTwo)
