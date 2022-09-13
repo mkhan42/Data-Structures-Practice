b1246d0dc10debb67eca3b4c60968d3bd9baa264
mkhan42 <michallekhan9@gmail.com>
2022-09-12 23:09:33 -0400

    notes

---
+++ DS-PRACTICE-COLT-STEELE/stringCount.js
@@ -0,0 +1,12 @@
+//Write a function which takes in a string and returns counts of each character in the string
+
+/*
+//text cases
+charCount("aaaa") //{a:4}
+charCount("hellp") //{h:1, e:1, l:2, o:1}
+
+"my phone number is 182763"
+"Hello hi" //counting unpercase and lowercase?
+
+charCount("") //what are we returning?
+*/
