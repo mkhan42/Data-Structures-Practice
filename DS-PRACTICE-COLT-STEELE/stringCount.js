90debdb0991938dd279de6bf2948829c438d619d
mkhan42 <michallekhan9@gmail.com>
2022-10-02 23:02:17 -0400

    string problem

--- DS-PRACTICE-COLT-STEELE/stringCount.js
+++ DS-PRACTICE-COLT-STEELE/stringCount.js
@@ -11,10 +11,33 @@ charCount("hellp") //{h:1, e:1, l:2, o:1}
 charCount("") //what are we returning?
 */
 
+
 function charCount(str) {
     //do something
     //return an object with keys that are lowercase alphanumeric characters in the string; values should be the counts
     //make object to return at end
     //loop over string
     //return object at end
+
+    let count = {}
+
+    let myStr = str.split('')
+
+    const letterFreq = myStr.forEach((element) => {
+        // count[element] = count[element] ? count[element] + 1 : 1;
+
+        element = element.toLowerCase()
+
+        if(count[element]) {
+            count[element] = count[element] + 1;
+        }
+
+        else {
+            count[element] = 1;
+        }
+    })
+
+   return count
 }
+
+console.log(charCount('hello my Name is n...'))
